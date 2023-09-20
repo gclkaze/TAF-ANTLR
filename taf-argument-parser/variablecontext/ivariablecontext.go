3469c00686df9430d49bc498e19a860590eb27c8
Mihalis Dorgiakis <michail.dorgiakis@gmail.com>
2023-09-20 18:23:05 +0300

    out

--- taf-argument-parser/variablecontext/ivariablecontext.go
+++
@@ -1,7 +0,0 @@
-package variablecontext
-
-type IVariableContext interface {
-	Init(isVerbose bool)
-	GetVariableIntValue(s string) float64
-	EvaluateJSONVariableIntValue(s string, path string) float64
-}
